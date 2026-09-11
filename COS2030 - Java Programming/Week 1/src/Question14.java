7f1bd700b6c6bd1b6c4bd6c9a77a2b2694580d1a
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-09-11 19:58:48 +0300

    Added solution to Problem 14

---
+++ COS2030 - Java Programming/Week 1/src/Question14.java
@@ -0,0 +1,13 @@
+public class Question14 {
+    public static void main(String[] args) {
+        System.out.println("+----------------------------+\n|      AUBG COFFEE BAR       |\n+----------------------------+");
+        System.out.println("Item            Qty     Price");
+        System.out.println("Espresso        2       3.00");
+        System.out.println("Croissant       1       2.50");
+        System.out.println();
+        System.out.println("Cashier said: \"Thank you!\"");
+        System.out.println("Saved to: C:\\receipts\\2026\\september.txt");
+        System.out.println("A backslash on its own: \\");
+        System.out.println("A tab between\tthese two words.");
+    }
+}
