8447a7f0ab8eec0b0106a6106badbf780dd308f9
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-09-11 19:32:26 +0300

    Added solution to Problem 8

---
+++ COS2030 - Java Programming/Week 1/src/Question08.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Question08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        System.out.println("You entered: " + number);
+    }
+}
