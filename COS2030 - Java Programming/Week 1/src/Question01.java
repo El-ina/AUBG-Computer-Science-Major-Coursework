63632b2ecee7b8092e8b20c7a54b79102e9cf022
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-09-11 17:38:46 +0300

    Initial commit for Java

---
+++ COS2030 - Java Programming/Week 1/src/Question01.java
@@ -0,0 +1,7 @@
+public class Question01 {
+    public static void main(String[] args){
+        System.out.println("Name: Elina Malchinikolova");
+        System.out.println("Majors: Computer Science & Business Administration");
+        System.out.println("Fun fact: I have played competitive table tennis");
+    }
+}
