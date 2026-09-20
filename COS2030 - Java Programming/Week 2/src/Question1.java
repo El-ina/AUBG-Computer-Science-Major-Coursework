b67f6928800fdcc511e853ee5437b590704a9c7f
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-09-20 21:15:38 +0300

    Added solution to Week 2 Problem 1

--- COS2030 - Java Programming/Week 2/src/Question1.java
+++ COS2030 - Java Programming/Week 2/src/Question1.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Question1{
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter filing status (0-3): ");
+        int filingStatus = scanner.nextInt();
+
+        if(filingStatus < 0 || filingStatus > 3){
+            System.out.println("Invalid filing status. Enter a value between 0 and 3.");
+            System.exit(1);
+        }
+
+        System.out.print("Enter taxable income: ");
+        double annualTaxableIncome = scanner.nextDouble();
+
+        double tax;
+
+        if (annualTaxableIncome <= 12_400)
+            tax = 0.10 * annualTaxableIncome;
+        else if (annualTaxableIncome <= 50_400)
+            tax = 1_240 + 0.12 * (annualTaxableIncome - 12_400);
+        else if (annualTaxableIncome <= 105_700)
+            tax = 5_800 + 0.22 * (annualTaxableIncome - 50_400);
+        else if (annualTaxableIncome <= 201_775)
+            tax = 17_966 + 0.24 * (annualTaxableIncome - 105_700);
+        else if (annualTaxableIncome <= 256_225)
+            tax = 41_024 + 0.32 * (annualTaxableIncome - 201_775);
+        else if (annualTaxableIncome <= 640_600)
+            tax = 58_448 + 0.35 * (annualTaxableIncome - 256_225);
+        else
+            tax = 192_979.25 + 0.37 * (annualTaxableIncome - 640_600);
+
+        System.out.printf("Tax: %.2f%n", tax);
     }
 }
